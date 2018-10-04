12f00f17d2489b97dbf58d033f053a4832d3b486
sakshi7373 <sakshi7373@gmail.com>
2018-10-03 21:44:18 -0700

    Search functionality added

---
+++ search.vue
@@ -0,0 +1,18 @@
+<template>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      placeholder: 'Search Movies',
+      results: {},
+      query: '',
+      posterBase: 'https://image.tmdb.org/t/p/w185/',
+      error: ''
+    }
+  },
+  getResults () {
+    var endpoint = 'https://fierce-gorge-62693.herokuapp.com/api/v1/moviedb/search/?format=json&query=' + this.query
+  }
+}
+</script>
