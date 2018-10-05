06b5899f4a6f3301882364e9557f821be62d00c2
pbah1 <pbahl1@asu.edu>
2018-10-05 11:08:58 -0700

    Adding this file to store constants used in project

---
+++ frontend/EnvironmentVariables.env.js
@@ -0,0 +1,5 @@
+// This file contains all constants used throughout the project.
+module.exports = {
+  POSTER_BASE_PATH: '"https://image.tmdb.org/t/p/w185/"',
+  QUERY: '"https://fierce-gorge-62693.herokuapp.com/api/v1/moviedb/search/?format=json&query="'
+}
